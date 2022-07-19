6b4531271de8f55c882c8aac35a006e29042f98b
DinaFH <dinaharb46@gmail.com>
2022-07-20 00:33:56 +0200

    remove uncompleted lab

--- Rails-Book-Store/app/controllers/admins_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdminsController < ApplicationController
-  def show
-    @admin = Admin.find(params[:id])
-    p @admin
-  end
-end
--- Rails-Book-Store/app/models/admin.rb
+++
@@ -1,7 +0,0 @@
-class Admin < ApplicationRecord
-  has_many :products, dependent: :destroy
-
-  def name_with_initial
-    "#{name}"
-  end
-end
